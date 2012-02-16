352d7b411e31f25e8be68bf38dbf5fc605adeacd
jazzs3quence <jazzs3quence@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-16 20:43:23 +0000

    adds 1.0.2 tag
    git-svn-id: https://plugins.svn.wordpress.org/progress-bar/tags/1.0.2@506159 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-progress-bar.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Plugin Name: Progress Bar
+Plugin URI: https://github.com/jazzsequence/progress-bar
+Description: a simple progress bar shortcode that can be styled with CSS
+Version: 1.0.2
+Author: Chris Reynolds
+Author URI: http://museumthemes.com
+License: GPL3
+*/
+
+/*
+	Progress Bar
+    Copyright (C) 2012 | Chris Reynolds (chris@arcanepalette.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    http://www.opensource.org/licenses/gpl-3.0.html
+*/
+
+/**
+ * wppb_init
+ * loads the css and javascript
+ * @author Chris Reynolds
+ * @since 0.1
+ */
+
+function wppb_init() {
+	$wppb_path = plugin_dir_url( __FILE__ );
+	if ( !is_admin() ) { // don't load this if we're in the backend
+		wp_register_style( 'wppb_css', $wppb_path . 'css/wppb.css' );
+		wp_enqueue_style( 'wppb_css' );
+		wp_enqueue_script( 'jquery' );
+		wp_register_script( 'wppb_animate', $wppb_path . 'js/wppb_animate.js', 'jquery' );
+		wp_enqueue_script ( 'wppb_animate' );
+	}
+}
+add_action( 'init', 'wppb_init' );
+
+/**
+ * Progress Bar
+ * simple shortcode that displays a progress bar
+ * @author Chris Reynolds
+ * @since 0.1
+ * @param string $progress REQUIRED displays the actual progress bar in % | usage [wppb progress=50]
+ * @param string $option OPTIONAL calls various options. These can be user-input (uses CSS classes, so anything a user adds to their CSS could potentially be used as an option) or any of the pre-defined options/styles. Included options (as of 1.0.1): candystripes, animated-candystripes, red | usage [wppb progress=50 option="red candystripes"] [wppb progress=50 option=animated-candystripes]
+ * @param string $percent OPTIONAL displays the percentage either on the bar itself, or after the progress bar, depending on which parameter is used. Options are 'after' and 'inside'. | usage [wppb progress=50 percent=after]
+ */
+
+function wppb( $atts ) {
+	extract( shortcode_atts( array(
+		'progress' => '',	// the progress in %
+		'option' => '',		// what options you want to use (candystripes, animated-candystripes, red)
+		'percent' => ''		// whether you want to display the percentage and where you want that to go (after, inside)
+		), $atts ) );
+	// here's the html output of the progress bar
+	$wppb_output	= "<div class=\"wppb-wrapper {$percent}\">"; // adding $percent to the wrapper class, so I can set a width for the wrapper based on whether it's using div.wppb-wrapper.after or div.wppb-wrapper.inside or just div.wppb-wrapper
+	if ($atts['percent'] != '') { // if $percent is not empty, add this
+		$wppb_output .= "<div class=\"{$percent}\">{$progress}%</div>";
+	}
+	$wppb_output 	.= 	"<div class=\"wppb-progress\">";
+	$wppb_output 	.=	"	<span style=\"width: {$progress}%;\" class=\"{$option}\"><span></span></span>";
+	$wppb_output	.=	"</div>";
+	$wppb_output	.= "</div>";
+	// now return the progress bar
+	return $wppb_output;
+}
+add_shortcode('wppb','wppb');
+
+?>
